95c0efe2dbf2ec215eb7e914f85c51e8344be33d
Alexandr Stanislavovich <hideqq100@gmail.com>
2024-01-24 18:14:03 +0300

    Added reworked tasks

--- Задача 1/Lecture 11 задача 1.py
+++ Задача 1/Lecture 11 задача 1.py
@@ -1,15 +1,15 @@
 # Задача 1
 class Soda:
 
-    def __init__(self, name):
+    def __init__(self, name=""):
         self.name = name
 
     def exam(self):
         if self.name == "":
-            return print("У вас обычная газировка")
+            return "У вас обычная газировка"
         else:
-            return print(f"Ваша газировка имеет {self.name} вкус")
+            return f"Ваша газировка имеет {self.name} вкус"
 
 
-test = Soda("лимонный")
-test.exam()
+test = Soda()
+print(test.exam())
--- Задача 5/Lecture 11 задача 5.py
+++ Задача 5/Lecture 11 задача 5.py
@@ -1,23 +1,21 @@
 # Задача 5
 class SuperStr(str):
-    def __init__(self, s):
-        self.s = s
-        super().__init__()
-
-    def is_repeatance(self, s_str):
-        value = len(self.s) % len(s_str)
-        if value == 0:
-            return True
-        else:
+    def is_repeatance(self, s):
+        if not isinstance(s, str):
             return False
+        else:
+            if (len(self) % len(s)) == 0:
+                return True
+            else:
+                return False
 
     def is_palindrome(self):
-        if self.s.lower() == self.s[::-1].lower():
+        if self.lower() == self[::-1].lower():
             return True
         else:
             return False
 
 
-test = SuperStr('qwertytrewq')
+test = SuperStr('qwertytrew')
 print(test.is_palindrome())
-print(test.is_repeatance("1234"))
+print(test.is_repeatance("12"))
